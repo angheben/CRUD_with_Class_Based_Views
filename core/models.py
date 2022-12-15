9027a06ea7bdc2425144778dc47001f093cac2b3
Vitor Angheben <vitor_angheben@hotmail.com>
2022-12-14 21:40:32 -0300

    Today I add a category of products to test some options

--- core/models.py
+++ core/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(name='name', max_length=100)
     price = models.DecimalField(name='price', max_digits=8, decimal_places=2)
+    category = models.CharField(name='category', max_length=100, default='None')
 
     def __str__(self):
         return self.name
